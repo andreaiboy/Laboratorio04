5b6d653c859600734546f3b0c289dee5b8f5646c
Andrea <andreagiboy@gmail.com>
2023-03-18 11:57:05 -0600

    Resolver el ejercicio 2

--- Laboratorio04/Ejercicio02.cs
+++ Laboratorio04/Ejercicio02.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,16 @@ namespace Laboratorio04
     {
         public double[] Calcular(int[] nums)
         {
-            return new double[] {0.0, 0.0};
+            double suma = 0.0;
+
+            foreach (int num in nums)
+            {
+                suma = suma + num;
+            }
+
+            double promedio = suma / nums.Length;
+
+            return new double[] {suma, promedio};
         }
     }
 }
