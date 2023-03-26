2ff00e43bf4bdc237e65e49258a49a27f26b0501
Andrea <andreagiboy@gmail.com>
2023-03-25 20:38:55 -0600

    realizar ejercicio 04

--- Laboratorio04/Ejercicio04.cs
+++ Laboratorio04/Ejercicio04.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization.Formatters;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,13 @@ namespace Laboratorio04
     {
         public int[] CalcularBilletes(int cantidad, int[] billetes)
         {
-            return new int[] {};
+            int[] result = new int[billetes.Length];
+            for (int i = 0; i < billetes.Length; i++)
+            {
+                result[i] = cantidad / billetes[i];
+                cantidad = cantidad % billetes[i];
+            }
+            return result;
         }
     }
 }
